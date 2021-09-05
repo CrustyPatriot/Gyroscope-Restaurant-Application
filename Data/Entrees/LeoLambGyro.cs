a77358f3d3b1321577188ac4876482f59f325f0b
schex <schexnayderalex@yahoo.com>
2021-09-05 13:06:59 -0500

    changed class names

--- Data/Entrees/LeoLambGyro.cs
+++ Data/Entrees/LeoLambGyro.cs
@@ -12,7 +12,7 @@ namespace GyroScope.Data.Entrees
     /// </summary>
     public class LeoLambGyro
     {
-        public DonorMeat Meat = DonorMeat.Lamb;
+        public DonerMeat Meat = DonerMeat.Lamb;
         public bool Pita = true;
         public bool Tomato = true;
         public bool Onion = true;
@@ -33,13 +33,13 @@ namespace GyroScope.Data.Entrees
         {
             get
             {
-                if (Meat != DonorMeat.Lamb)
+                if (Meat != DonerMeat.Lamb)
                 {
-                    if (Meat == DonorMeat.Beef)
+                    if (Meat == DonerMeat.Beef)
                     {
                         yield return "Use beef";
                     }
-                    if (Meat == DonorMeat.Chicken)
+                    if (Meat == DonerMeat.Chicken)
                     {
                         yield return "Use chicken";
                     }
@@ -84,19 +84,19 @@ namespace GyroScope.Data.Entrees
             {
                 uint calorieSum = 0;
 
-                if (Meat == DonorMeat.Pork)
+                if (Meat == DonerMeat.Pork)
                 {
                     calorieSum += 187;
                 }
-                else if (Meat == DonorMeat.Lamb)
+                else if (Meat == DonerMeat.Lamb)
                 {
                     calorieSum += 151;
                 }
-                else if (Meat == DonorMeat.Chicken)
+                else if (Meat == DonerMeat.Chicken)
                 {
                     calorieSum += 113;
                 }
-                else if (Meat == DonorMeat.Beef)
+                else if (Meat == DonerMeat.Beef)
                 {
                     calorieSum += 181;
                 }
---
+++ Data/Sides/AriesFries.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GyroScope.Data.Enums;
+
+namespace GyroScope.Data.Sides
+{
+    /// <summary>
+    /// A class to represent a side of Ares Fries.
+    /// </summary>
+    public class AriesFries
+    {
+        /// <summary>
+        /// The size of the Ares Fries.
+        /// \
+        /// </summary>
+        public Size Size { get; set; }
+
+        /// <summary>
+        /// The price of the Ares Fries.
+        /// </summary>
+        public decimal Price
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 1.50m;
+                    case Size.Medium:
+                        return 2.00m;
+                    case Size.Large:
+                        return 2.50m;
+                    default:
+                        throw new NotImplementedException($"Unknown Size {Size}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// The calories of the Ares Fries.
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                switch (Size)
+                {
+                    case Size.Small:
+                        return 304;
+                    case Size.Medium:
+                        return 456;
+                    case Size.Large:
+                        return 608;
+                    default:
+                        throw new NotImplementedException($"Unknown Calorie amount");
+                }
+            }
+        }
+    }
+}
