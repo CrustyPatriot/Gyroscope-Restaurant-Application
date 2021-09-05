eb65e78ec13652b69484025eef57a0c332b5b7e4
schex <schexnayderalex@yahoo.com>
2021-09-05 10:15:59 -0500

    Added DonorMeats enum

---
+++ Data/Enums/DonorMeats.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GyroScope.Data.Enums
+{
+    /// <summary>
+    /// An enumeration representing the types of donor meats available.
+    /// </summary>
+    public enum DonorMeats
+    {
+        Beef,
+        Pork,
+        Chicken,
+        Lamb
+    }
+}
