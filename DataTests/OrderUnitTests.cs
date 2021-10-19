7158601ef1bbb9d774639e91e6b4934e22cf4897
CrustyPatriot <sexyschexy@ksu.edu>
2021-10-19 12:11:31 -0500

    Tests "finished"

--- DataTests/OrderUnitTests.cs
+++ DataTests/OrderUnitTests.cs
@@ -3,7 +3,9 @@ using Xunit;
 using GyroScope.Data.Enums;
 using GyroScope.Data.Treats;
 using System.ComponentModel;
-
+using GyroScope.Data;
+using GyroScope.Data.Entrees;
+using System.Collections.Specialized;
 
 namespace GyroScope.DataTests
 {
@@ -12,6 +14,49 @@ namespace GyroScope.DataTests
     /// </summary>
     public class OrderUnitTests
     {
+        /// <summary>
+        /// Checks to see if the added item is included in the order.
+        /// </summary>
+        [Fact]
+        public void CheckIfAddedItemsAreIncludedInOrder()
+        {
+            Order o = new();
+            LeoLambGyro gyro = new();
+            o.Add(gyro);
+            Assert.Contains(gyro,o);
+        }
 
+        /// <summary>
+        /// Checks to see if the added and removed item is not included in the order.
+        /// </summary>
+        [Fact]
+        public void CheckIfRemovedItemsAreNotIncludedInOrder()
+        {
+            Order o = new();
+            LeoLambGyro gyro = new();
+            o.Add(gyro);
+            o.Remove(gyro);
+            Assert.DoesNotContain(gyro, o);
+        }
+
+        /// <summary>
+        /// Checks to see that the INotifyPropertyChanged event is correctly changed.
+        /// </summary>
+        [Fact]
+        public void ShouldImplementINotifyPropertyChanged()
+        {
+            Order o = new();
+            Assert.IsAssignableFrom<INotifyPropertyChanged>(o);
+        }
+
+        /// <summary>
+        /// Checks to see that the INotifyCollectionChanged event is correctly changed.
+        /// </summary>
+        [Fact]
+        public void ShouldImplementINotifyCollectionChanged()
+        {
+            Order o = new();
+            Assert.IsAssignableFrom<INotifyCollectionChanged>(o);
+        }
     }
 }
